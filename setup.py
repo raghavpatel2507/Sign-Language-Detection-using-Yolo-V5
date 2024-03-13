ad3c6e709a52401d186bd39ebd577b226999d96d
raghav patel <patelraghav014@gmail.com>
2024-03-13 21:38:28 +0530

    requirments added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Sign Language Detection',
+    version= '0.0.0',
+    author= 'Raghav Patel',
+    author_email= 'patelraghav014@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
