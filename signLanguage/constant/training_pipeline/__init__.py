d32af053f2300ea53370b3ff4c58f8c444e84c32
raghav patel <patelraghav014@gmail.com>
2024-03-15 21:27:46 +0530

    data validation added

--- signLanguage/constant/training_pipeline/__init__.py
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -10,4 +10,28 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
-DATA_DOWNLOAD_URL: str = "https://github.com/raghavpatel2507/Datasets/raw/main/signlanguage.zip"
+DATA_DOWNLOAD_URL: str = "https://github.com/raghavpatel2507/Datasets/raw/main/signlanguage_data.zip"
+
+
+"""
+Data Validation realted contant 
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "test", "data.yaml"]
+
+"""
+MODEL TRAINER related constant 
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+
+MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov5s.pt"
+
+MODEL_TRAINER_NO_EPOCHS: int = 15   # you can go more epochs.
+
+MODEL_TRAINER_BATCH_SIZE: int = 16
+
+
