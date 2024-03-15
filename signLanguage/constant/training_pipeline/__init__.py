57d0a97e051ff2d08762dcb906ad77178b6d988d
raghav patel <patelraghav014@gmail.com>
2024-03-15 11:04:26 +0530

    constant update

--- signLanguage/constant/training_pipeline/__init__.py
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -10,4 +10,4 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
-DATA_DOWNLOAD_URL: str = "https://github.com/raghavpatel2507/Datasets/raw/main/Sign%20Language%20Detection.v1i.yolov5pytorch.zip"
+DATA_DOWNLOAD_URL: str = "https://github.com/raghavpatel2507/Datasets/raw/main/signlanguage.zip"
